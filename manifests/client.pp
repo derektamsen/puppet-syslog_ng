703fbb3abbc469c59bea421ea47519259b3484c0
Derek Tamsen <tamsen@squareup.com>
2014-03-26 15:10:59 -0700

    changing quotes to single where vars are not in use

--- manifests/client.pp
+++ manifests/client.pp
@@ -8,10 +8,10 @@
 class syslog_ng::client inherits syslog_ng {
 
     file { 
-        "/etc/syslog-ng/syslog-ng.conf":
-            notify  => Service["syslog-ng"],
-            content => template("syslog_ng/syslog-ng.conf.client.erb");
-        "/etc/logrotate.d/syslog":
-            source => "puppet:///modules/syslog_ng/syslog-logrotate";
+        '/etc/syslog-ng/syslog-ng.conf':
+            notify  => Service['syslog-ng'],
+            content => template('syslog_ng/syslog-ng.conf.client.erb');
+        '/etc/logrotate.d/syslog':
+            source => 'puppet:///modules/syslog_ng/syslog-logrotate';
     } # file
 } # class syslog_ng::client
